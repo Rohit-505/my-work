d827505bef24ff5862200622f8335e6ebe58693f
Rohit Vishwakarma <84259507+Rohit-505@users.noreply.github.com>
2021-07-13 00:37:15 +0530

    Update MAIN.java

--- MAIN.java
+++ MAIN.java
@@ -7,6 +7,12 @@ public class MAIN {
 		b1.insert(10);
 		b1.insert(20);
 		b1.insert(15);
-		
+		b1.insert(27);
+		b1.insert(13);
+		b1.insert(9);
+		b1.insert(3);
+		b1.insert(5);
+		b1.insert(8);
+		b1.inorder();
 	}
 }
