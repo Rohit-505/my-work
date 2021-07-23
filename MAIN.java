d145323c92cf787637ac49d5ce2fe7875c62518a
Rohit Vishwakarma <84259507+Rohit-505@users.noreply.github.com>
2021-07-23 10:55:15 +0530

    Add files via upload

--- MAIN.java
+++ MAIN.java
@@ -1,18 +1,38 @@
 package NonLinear;
-import java.util.Scanner;
-public class MAIN {
-
+import java.util.*;
+public class MAIN{
 	public static void main(String[]args) {
-		BST b1=new BST();
-		b1.insert(10);
-		b1.insert(20);
-		b1.insert(15);
-		b1.insert(27);
-		b1.insert(13);
-		b1.insert(9);
-		b1.insert(3);
-		b1.insert(5);
-		b1.insert(8);
-		b1.inorder();
+		
+	//	Scanner sc=new Scanner(System.in);
+		//System.out.println("enter number of vertex");
+		//int v=sc.nextInt();
+		Graph g=new Graph(5);
+		
+			g.AddVertex("A",0);
+			g.AddVertex("B",1);
+			g.AddVertex("C",2);
+			g.AddVertex("D",3);
+			g.AddVertex("E",4);
+			
+			g.AddEdge("A","B");
+			g.AddEdge("A","C");
+			g.AddEdge("A","D");
+			
+			g.AddEdge("B","A");
+			g.AddEdge("B","C");
+			g.AddEdge("B","D");
+			
+			g.AddEdge("C","A");
+			g.AddEdge("C","B");
+			g.AddEdge("C","D");
+			g.AddEdge("C","E");
+			
+			g.AddEdge("E","C");
+			g.AddEdge("E","D");
+			
+			g.getconnection("C");
+			
+			
+		
 	}
 }
